a31e1f9be78bd677b2d50b5ab7a4add2ca8375c6
eynsmre <yerkeles@gmail.com>
2024-12-02 16:40:15 +0300

    Product Carousel component ve Buton düzenlemesi

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,22 @@
 <template>
   <div>
     <div>
-      <button class= ""v-for="component in components" :key="component.name" @click="showComponent(component.name)">
+      <button
+        class=""
+        v-for="component in components"
+        :key="component.name"
+        @click="showComponent(component.name)"
+      >
         {{ component.label }}
       </button>
     </div>
 
     <div>
       <ComponentA v-if="currentComponent === 'ComponentA'" />
-      <ComponentB v-if="currentComponent === 'ComponentB'" />
       <ComponentC v-if="currentComponent === 'ComponentC'" />
       <ComponentD v-if="currentComponent === 'ComponentD'" />
       <ComponentE v-if="currentComponent === 'ComponentE'" />
       <ComponentF v-if="currentComponent === 'ComponentF'" />
-      <ComponentG v-if="currentComponent === 'ComponentG'" />
       <ComponentH v-if="currentComponent === 'ComponentH'" />
       <ComponentI v-if="currentComponent === 'ComponentI'" />
       <ComponentJ v-if="currentComponent === 'ComponentJ'" />
@@ -22,53 +25,47 @@
 </template>
 
 <script>
-import ComponentA from '~/components/ComponentA.vue'
-import ComponentB from '~/components/ComponentB.vue'
-import ComponentC from '~/components/ComponentC.vue'
-import ComponentD from '~/components/ComponentD.vue'
-import ComponentE from '~/components/ComponentE.vue'
-import ComponentF from '~/components/ComponentF.vue'
-import ComponentG from '~/components/ComponentG.vue'
-import ComponentH from '~/components/ComponentH.vue'
-import ComponentI from '~/components/ComponentI.vue'
-import ComponentJ from '~/components/ComponentJ.vue'
+import ComponentA from "~/components/ComponentA.vue";
+import ComponentC from "~/components/ComponentC.vue";
+import ComponentD from "~/components/ComponentD.vue";
+import ComponentE from "~/components/ComponentE.vue";
+import ComponentF from "~/components/ComponentF.vue";
+import ComponentH from "~/components/ComponentH.vue";
+import ComponentI from "~/components/ComponentI.vue";
+import ComponentJ from "~/components/ComponentJ.vue";
 
 export default {
   components: {
     ComponentA,
-    ComponentB,
     ComponentC,
     ComponentD,
     ComponentE,
     ComponentF,
-    ComponentG,
     ComponentH,
     ComponentI,
-    ComponentJ
+    ComponentJ,
   },
   data() {
     return {
-      currentComponent: '',
+      currentComponent: "",
       components: [
-        { name: 'ComponentA', label: 'Buton1' },
-        { name: 'ComponentB', label: 'Buton2' },
-        { name: 'ComponentC', label: 'Buton3' },
-        { name: 'ComponentD', label: 'Buton4' },
-        { name: 'ComponentE', label: 'Buton5' },
-        { name: 'ComponentF', label: 'Buton6' },
-        { name: 'ComponentG', label: 'Buton7' },
-        { name: 'ComponentH', label: 'Buton8' },
-        { name: 'ComponentI', label: 'Buton9' },
-        { name: 'ComponentJ', label: 'Buton10' }
-      ]
-    }
+        { name: "ComponentA", label: "Buton1" },
+        { name: "ComponentC", label: "Buton3" },
+        { name: "ComponentD", label: "Buton4" },
+        { name: "ComponentE", label: "Buton5" },
+        { name: "ComponentF", label: "Buton6" },
+        { name: "ComponentH", label: "Buton8" },
+        { name: "ComponentI", label: "Buton9" },
+        { name: "ComponentJ", label: "Buton10" },
+      ],
+    };
   },
   methods: {
     showComponent(componentName) {
-      this.currentComponent = componentName
-    }
-  }
-}
+      this.currentComponent = componentName;
+    },
+  },
+};
 </script>
 
 <style scoped>
